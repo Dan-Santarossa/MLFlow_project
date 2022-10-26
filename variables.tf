d810b0b8bc93e457a9c8ac874a2f620e7fff999a
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-26 17:37:51 -0400

    created unique_name variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "tags" {
   default     = {}
   description = "AWS Tags common to all the resources created"
 }
+
+variable "unique_name" {
+  type        = string
+  description = "A unique name for this application"
+  default     = "oauth2-mlflow"
+}
