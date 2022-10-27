eb690be42890614f952b9aa172e0f5fbe3b44b1f
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-27 14:51:07 -0400

    rds cluster/mlflow backend block

--- rds.tf
+++ rds.tf
@@ -15,3 +15,26 @@ resource "aws_db_subnet_group" "rds" {
   subnet_ids = local.db_subnet_ids
 }
 
+resource "aws_rds_cluster" "mlflow_backend_store" {
+  cluster_identifier        = "${local.name}-rds"
+  engine                    = "aurora-postgresql"
+  engine_mode               = "serverless"
+  port                      = local.db_port
+  db_subnet_group_name      = aws_db_subnet_group.rds.name
+  vpc_security_group_ids    = [aws_security_group.mlflow_server_sg.0.id]
+  availability_zones        = local.availability_zones
+  database_name             = local.db_database
+  master_username           = local.db_username
+  master_password           = random_password.mlflow_backend_store.result
+  backup_retention_period   = 5
+  preferred_backup_window   = "04:00-06:00"
+  final_snapshot_identifier = "mlflow-db-backup"
+  deletion_protection       = var.db_deletion_protection
+  apply_immediately         = true
+
+  scaling_configuration {
+    min_capacity             = var.db_min_capacity
+    max_capacity             = var.db_max_capacity
+    auto_pause               = var.db_auto_pause
+    seconds_until_auto_pause = var.db_auto_pause_seconds
+  }
