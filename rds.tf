81f3102ef859399a231055d0fcd221ee1ab21d04
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-27 12:35:01 -0400

    invalid reference in validation:line 12, 13, 15 rds.tf

--- rds.tf
+++ rds.tf
@@ -7,5 +7,17 @@ resource "aws_security_group" "rds" {
   name   = "${var.unique_name}-rds"
   vpc_id = module.vpc.vpc_id
   tags   = local.tags
-}
 
+ingress {
+    from_port       = db_port
+    to_port         = db_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_service.id]
+}
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
