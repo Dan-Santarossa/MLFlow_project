a10591cec42dce2070c398ccc383756ca956ee41
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-27 12:32:40 -0400

    rds sec group creation

--- rds.tf
+++ rds.tf
@@ -2,3 +2,10 @@ resource "aws_db_subnet_group" "rds" {
   name       = "${var.unique_name}-rds"
   subnet_ids = module.vpc.database_subnets
 }
+
+resource "aws_security_group" "rds" {
+  name   = "${var.unique_name}-rds"
+  vpc_id = module.vpc.vpc_id
+  tags   = local.tags
+}
+
