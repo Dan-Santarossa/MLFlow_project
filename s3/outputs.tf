97c5b642c8a43e24375ae5e78821dd6c89e543e3
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-27 14:25:09 -0400

    moving along

---
+++ s3/outputs.tf
@@ -0,0 +1,9 @@
+## -- s3/outputs.terraform 
+
+output "artifact_bucket_id" {
+  value = aws_s3_bucket.mlflow_artifact_store.0.id
+}
+
+output "artifact_bucket_arn" {
+  value = aws_s3_bucket.mlflow_artifact_store.*.arn
+}
