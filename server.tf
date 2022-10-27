ab8f3dbc588fe277f515547b6b212815863ca7a7
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-27 15:57:17 -0400

    fix unclosed block after config

--- server.tf
+++ server.tf
@@ -3,3 +3,19 @@ resource "random_password" "mlflow_password" {
   special          = true
   override_special = "_%@"
 }
+
+resource "aws_apprunner_service" "mlflow_server" {
+  service_name = "${local.name}"
+
+  source_configuration {
+    auto_deployments_enabled = false
+
+    image_repository {
+      image_identifier      = "public.ecr.aws/t9j8s4z8/mlflow:1.30.0"
+      image_repository_type = "ECR_PUBLIC"
+image_configuration {
+        port = local.app_port
+    }
+  }
+}
+
