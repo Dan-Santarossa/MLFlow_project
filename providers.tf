ff934cb55edd307ccb33daac55312a36a9ff598e
Dan-Santarossa <dpsantarossa@gmail.com>
2022-10-30 10:59:12 -0400

    state backend

--- providers.tf
+++ providers.tf
@@ -8,8 +8,8 @@ terraform {
 
   required_version = ">= 1.3"
 
-  backend "s3" {
-    # bucket = "mlflow-state"
+  backend "s3" {            #use your own bucket name for state storage
+    bucket = "mlflow-state" #having trouble creating back end - remade an s3 bucket manually for s3 state storage - not managed by terraform
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
